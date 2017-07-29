092164f6445ae885ef0530cc633c886d48e3ace3
dani1373 <dmehrjerdi@gmail.com>
2017-07-29 17:02:48 +0430

    tests added

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ class Test
     public static void main(String[] args)
     {
         testSum();
+        testMul();
     }
 
     private static void testSum()
@@ -19,6 +20,23 @@ class Test
         else
         {
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
+    
+    private static void testMul()
+    {
+        int a = 5;
+        int b = 6;
+        int expected = 30;
+        BasicMath bm = new BasicMath();
+        int result = bm.multiply(a, b);
+        if (result == expected)
+        {
+            System.out.println("Multiply: OK");
+        }
+        else
+        {
+            System.out.println("Multiply: Failed, expected = " + expected + ", result = " + result);
         }
     }
 }
